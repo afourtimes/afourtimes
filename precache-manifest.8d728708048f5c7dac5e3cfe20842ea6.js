4d4a9b3fe61d6d2fd6bf2f5ff4491c4cd6caae19
Aditya Astono <aditya.astono@debijenkorf.nl>
2019-03-03 21:47:16 +0100

    Updates

---
+++ precache-manifest.8d728708048f5c7dac5e3cfe20842ea6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/afourtimes/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6b56b3b0665dbd45b031",
+    "url": "/afourtimes/static/js/runtime~main.6b56b3b0.js"
+  },
+  {
+    "revision": "3aacf53a9a6eba44483b",
+    "url": "/afourtimes/static/js/main.3aacf53a.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/afourtimes/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "3aacf53a9a6eba44483b",
+    "url": "/afourtimes/static/css/main.9311231f.chunk.css"
+  },
+  {
+    "revision": "b10fec1be7f1fd75e2c42cd2c9d6801e",
+    "url": "/afourtimes/index.html"
+  }
+];
