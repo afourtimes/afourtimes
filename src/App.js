563be09d292250541b206aa34bfc12435d16c131
Aditya Astono <aditya.astono@debijenkorf.nl>
2019-03-08 22:43:28 +0100

    Hi there

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 class App extends Component {
     state = {
         windowWidth: window.innerWidth,
+        time: 0,
     }
 
     handleResize = () => {
@@ -13,7 +14,14 @@ class App extends Component {
         });
     }
 
+    startTimer = () => {
+        setInterval(() => this.setState({
+            time: this.state.time + 1
+        }), 1000);
+    }
+
     componentDidMount() {
+        this.startTimer();
         window.addEventListener('resize', this.handleResize);
     }
 
@@ -24,37 +32,60 @@ class App extends Component {
     render() {
         const {windowWidth} = this.state;
         const size = {
-            width: windowWidth < 767 ? '100%' : 300,
+            width: windowWidth < 800 ? '100%' : 300,
             height: 425,
         };
-        const view = 'list';
-        const theme = 'white';
 
         return (
             <div className="App">
-                <img
-                    src={require('./assets/img/AAAA.jpg')}
-                    width="100"
-                    height="100"
-                    alt="AAAA-logo"
-                    style={{display: 'block', margin: '0 auto'}}
-                    id="offset"
-                />
-                <SpotifyPlayer
-                    uri="spotify:user:adityaastono:playlist:0lCME6JLnPqInwv8SdW2jQ"
-                    size={size}
-                    view={view}
-                    theme={theme}
-                />
-                <a href="https://www.instagram.com/afourtimes/">
-                    <img
-                        src={require('./assets/img/ig-logo.png')}
-                        width="24"
-                        height="24"
-                        alt="ig-logo"
-                        style={{display: 'block', margin: '20px auto'}}
+                <header className="afourtimes-logo">
+                    <div className="logo-wrapper">
+                        <img
+                            src={require('./assets/img/AAAA.jpg')}
+                            width="100"
+                            height="100"
+                            alt="AAAA-logo"
+                            style={{display: 'block', margin: '0 auto'}}
+                            id="offset"
+                        />
+                    </div>
+                </header>
+                <div className="spotify-player">
+                    <SpotifyPlayer
+                        uri="spotify:user:adityaastono:playlist:0lCME6JLnPqInwv8SdW2jQ"
+                        size={size}
                     />
-                </a>
+                </div>
+
+                <div className="aside aside-1 align-right">
+                    <p>
+                        <span className="timer-number">{this.state.time}</span>s
+                    </p>
+                </div>
+
+                <div className="aside aside-2 align-left">
+                    <p style={ { marginBottom: '30px' } }>
+                        An eye for an eye.<br/>
+                        A heart for a heart.<br/>
+                        Perhaps this is goodbye.<br/>
+                        Shall us not apart.
+                    </p>
+                    <p>
+                        - <i>A. Astono</i>
+                    </p>
+                </div>
+
+                <div className="footer">
+                    <a href="https://www.instagram.com/afourtimes/">
+                        <img
+                            className="ig-logo"
+                            src={require('./assets/img/ig-logo.png')}
+                            width="24"
+                            height="24"
+                            alt="ig-logo"
+                        />
+                    </a>
+                </div>
 
             </div>
         );
